5f987f92625e290882d47d0c1e138f375e55c497
prakhar17072004 <pmankur2004@gmail.com>
2025-02-27 00:50:42 +0530

    new  components is added

--- src/app/User/page.tsx
+++ src/app/User/page.tsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+
+
 
 function User() {
   return (
     <div>
         <Navbar/>
-        <h1>user</h1>
+   <div className='className="min-h-screen bg-gray-50 p-8'>
+   <h1 className="text-3xl font-bold mb-6">User Services</h1>
+   <Tabs defaultValue="account" className="">
+  <TabsList>
+    <TabsTrigger value="account" >Services Available</TabsTrigger>
+    <TabsTrigger value="password">Apply services</TabsTrigger>
+  </TabsList>
+  <TabsContent value="account">
+    <ul>
+      <li >Rent Agreement
+        <span className='ml-8'>
+              <Button variant="outline">Apply</Button>
+        </span>
+
+      </li>
+      <li >Rent Agreement
+        <span className='ml-8'>
+              <Button variant="outline">Apply</Button>
+        </span>
+
+      </li>
+      <li >Rent Agreement
+        <span className='ml-8'>
+              <Button variant="outline">Apply</Button>
+        </span>
+
+      </li>
+      <li >Rent Agreement
+        <span className='ml-8'>
+              <Button variant="outline">Apply</Button>
+        </span>
+
+      </li>
+
+    </ul>
+  </TabsContent>
+  <TabsContent value="password">Change your password here.</TabsContent>
+</Tabs>
+   </div>
     </div>
   )
 }
