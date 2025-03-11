d0a070803dfae037a3f00de6eff7457fe4199170
prakhar17072004 <pmankur2004@gmail.com>
2025-03-11 16:14:32 +0530

    schemais updated

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,7 @@ export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   first_name: varchar("first_name", { length: 25 }).notNull(),
   last_name: varchar("last_name", { length: 25 }).notNull(),
-  mobile_number: varchar("mobile_number", { length: 15 }).notNull().unique(),
+  mobile_number: varchar("mobile_number", { length: 10 }).notNull().unique(),
   password: varchar("password", { length: 255 }).notNull(),
   role: varchar("role", { length: 10 }).notNull(), // Can be "user", "babu", or "admin"
 });
@@ -15,6 +15,8 @@ export const babus = pgTable("babus", {
   id: serial("id").primaryKey(),
   first_name: varchar("first_name", { length: 25 }).notNull(),
   last_name: varchar("last_name", { length: 25 }).notNull(),
+  mobile_number: varchar("mobile_number", { length: 10 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   babu_id: varchar("babu_id", { length: 20 }).notNull().unique(), // Unique Babu Identifier
 });
 
@@ -23,5 +25,7 @@ export const admins = pgTable("admins", {
   id: serial("id").primaryKey(),
   first_name: varchar("first_name", { length: 25 }).notNull(),
   last_name: varchar("last_name", { length: 25 }).notNull(),
+  mobile_number: varchar("mobile_number", { length: 10 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   admin_code: varchar("admin_code", { length: 20 }).notNull(), // Unique Admin Code
 });
