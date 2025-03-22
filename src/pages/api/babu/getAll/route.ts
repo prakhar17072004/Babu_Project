ae81256705d2fee7d0496963ca5ecc93f9b586c4
prakhar17072004 <pmankur2004@gmail.com>
2025-03-22 19:21:52 +0530

    loginpage update

--- src/pages/api/babu/getAll/route.ts
+++ src/pages/api/babu/getAll/route.ts
@@ -1,15 +1,15 @@
-import { NextResponse } from "next/server";
-import { db } from "@/app/db/index";
-import { babus } from "@/app/db/schema";
-import { eq } from "drizzle-orm"; // Import eq for filtering
+// import { NextResponse } from "next/server";
+// import { db } from "@/app/db/index";
+// import { babus } from "@/app/db/schema";
+// import { eq } from "drizzle-orm"; // Import eq for filtering
 
-export async function GET() {
-  try {
-    // Fetch users where role is "user"
-    const userList = await db.select().from(babus).where(eq(babus.role, "babu"));
+// export async function GET() {
+//   try {
+//     // Fetch users where role is "user"
+//     const userList = await db.select().from(babus).where(eq(babus.role, "babu"));
 
-    return NextResponse.json({ users: userList }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ users: userList }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
+//   }
+// }
--- src/pages/api/user/getAll/route.ts
+++ src/pages/api/user/getAll/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import { db } from "@/app/db/index";
-import { users } from "@/app/db/schema";
-import { eq } from "drizzle-orm"; // Import eq for filtering
+// import { NextResponse } from "next/server";
+// import { db } from "@/app/db/index";
+// import { users } from "@/app/db/schema";
+// import { eq } from "drizzle-orm"; // Import eq for filtering
 
-export async function GET() {
-  try {
-    // Fetch users where role is "user"
-    const userList = await db.select().from(users).where(eq(users.role, "user"));
+// export async function GET() {
+//   try {
+//     // Fetch users where role is "user"
+//     const userList = await db.select().from(users).where(eq(users.role, "user"));
 
-    return NextResponse.json({ users: userList }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ users: userList }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
+//   }
+// }
 
