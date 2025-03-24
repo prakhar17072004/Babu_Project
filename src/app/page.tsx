314f0421394abf72363590a0bb6b75232886af27
prakhar17072004 <pmankur2004@gmail.com>
2025-03-24 18:28:35 +0530

    dashboard updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Navbar from "@/components/Navbar";
 import Link from 'next/link';
 import Location from '@/components/Location';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
     <Navbar/>
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800 p-6">
         <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
-        <p className="text-lg mb-8">
+        <Link href="/Getstart">
+          <Button className='bg-green-800'>Getstarted</Button>
+        </Link>
+        {/* <p className="text-lg mb-8">
           Manage records for Users,Babus and Admin.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-2xl">
@@ -35,7 +39,7 @@ export default function Home() {
             <h2 className="text-2xl font-semibold mb-2">Admin &rarr;</h2>
             <p>View and manage admin records.</p>
           </Link>
-        </div>
+        </div> */}
       </div>
     
     </>
