b981c954768bc7139a635cf861577cafca15ac15
prakhar17072004 <pmankur2004@gmail.com>
2025-03-26 14:28:48 +0530

    favicon is updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LoginProvider>
+       <link rel="icon" href="/favicon.png" />
       <Component {...pageProps} />
     </LoginProvider>
   );
